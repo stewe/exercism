f3fd70fcc80a8d8969ba42caba311bd3b087c162
Stefan Wendt <stefanwendt@posteo.de>
2017-01-21 04:37:49 +0100

    java/nucleotide-count improvements

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,29 +1,25 @@
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 public class DNA {
 	private final String dna;
+    private List<Character> nucleotides = Arrays.asList('A', 'C', 'G', 'T');
+
 	
 	public DNA(String dna) {
 		this.dna = dna;
 	}
 	
 	public int count (char nuc) {
-		if (!(nuc == 'A' || nuc == 'C' || nuc == 'G' || nuc == 'T'))
-			throw new IllegalArgumentException();
+		if (!nucleotides.contains(nuc)) throw new IllegalArgumentException();
 		return (int) dna.chars().filter(c -> c == nuc).count();
 	}
 
 	public Map<Character, Integer> nucleotideCounts() {
 		Map<Character, Integer> counts = new HashMap<Character, Integer>();
-		counts.put('A', 0);
-		counts.put('C', 0);
-		counts.put('G', 0);
-		counts.put('T', 0);
-		for (char c : dna.toCharArray()) {
-			counts.compute(c, (k,v) -> ++v);
-		}
+		nucleotides.forEach(nuc -> counts.put(nuc, count(nuc)));
 		return counts;
 	}
 }
